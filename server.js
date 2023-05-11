8d0df717eae196ac34c341bfcb50d3a97173838a
MohamedEL-gantery <elgantery50@gmail.com>
2023-05-11 16:05:40 +0300

    push

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(
+  "mongodb://0.0.0.0:27017/hospital",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  },
+  (err) => {
+    if (!err) {
+      console.log("DB Success...");
+    } else {
+      console.log(err);
+    }
+  }
+);
+
+const port = 3333;
+app.listen(port, () => {
+  console.log(`App Running in port ${port}`);
+});
